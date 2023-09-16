aca3f769149a2c80db2f14844e62c6a07404e98d
l0newo1f <142143389+l0newo1f@users.noreply.github.com>
2023-09-16 14:11:37 +0800

    more contents concerning function

--- function.py
+++ function.py
@@ -30,6 +30,7 @@
 # muscian = get_formatted_name('lance','stroll')
 # print(muscian)
 
+
 #返回字典
 # def get_formatted_name(first_name,last_name,age=None):
 #     '''返回个人信息字典'''
@@ -44,25 +45,63 @@
 # muscian = get_formatted_name('lance','stroll',age=17)
 # print(muscian)
 
-#包含循环
-def get_formatted_name(first_name,last_name):
-    '''返回标准个人姓名'''
-    full_name = f"{first_name} {last_name}"
-    return full_name.title()
 
-print("Please tell us your name")
-print("Please enter 'q' any time you want to quit ")
+#使用循环的函数
+# def get_formatted_name(first_name,last_name):
+#     '''返回标准个人姓名'''
+#     full_name = f"{first_name} {last_name}"
+#     return full_name.title()
 
-while True:
-    first_name = input('First name: ')
-    if first_name == "q":
-        print('Thank you for playing this')
-        break
+# print("Please tell us your name")
+# print("Please enter 'q' any time you want to quit ")
+
+# while True:
+#     first_name = input('First name: ')
+#     if first_name == "q":
+#         print('Thank you for playing this')
+#         break
     
-    last_name = input('Last_name: ')
-    if last_name == "q":
-        print('Thank you for playing this')
-        break
+#     last_name = input('Last_name: ')
+#     if last_name == "q":
+#         print('Thank you for playing this')
+#         break
     
-    person = get_formatted_name(first_name,last_name)
-    print(f"Hello {person}!")
+#     person = get_formatted_name(first_name,last_name)
+#     print(f"Hello {person}!")
+
+
+#传递列表的函数
+def print_name(user_names):
+    for user_name in user_names:
+        print(f'Hello {user_name.title()} !')
+
+# names = ['eric','john','hame']
+# print_name(names)
+
+
+#在函数中修改列表
+
+
+#在修改后函数不改变原列表值
+#函数的实参使用list[:]即列表的副本，而不影响原列表
+
+
+#传递任意数量(元组)的实参
+def make_pizza(*toppings):
+    '''打印披萨所有配料'''
+    print("Making pizza with following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+
+# make_pizza('mushrooms','green peppers','cheese')
+# make_pizza('mushrooms')
+
+#传递任意数量(字典)的实参
+def build_profile(first,last,**user_info):
+    '''创建用户的字典信息'''
+    user_info['first'] = first
+    user_info['last'] = last
+    return user_info
+
+# user_profile = build_profile('albert','einstein',location = 'princeton',field = 'physics')
+# print(user_profile)
